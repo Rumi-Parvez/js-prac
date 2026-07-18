89548fcce026a3eec3d6d748f4d8f94577c1c339
Rumi Parvez <openyhoolceo@gmail.com>
2026-07-18 21:48:35 +0600

    loop

---
+++ loop.js
@@ -0,0 +1,18 @@
+// loop syntext 
+/**
+ * for( initioalization or variable the concept ; consition ;  effects like core wok like ++)
+ * 
+ */
+
+// for( let name = 0 ; name <= 1000 ; name ++ ){
+//     console.log("hit" + name)
+// }
+
+
+let i = 0;
+while(i<=100){
+    console.log("this is " + i)
+    i++
+}
+
+
